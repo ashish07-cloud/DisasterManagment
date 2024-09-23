1408234192fc19fd45c41bb6a8d55a92428a2803
ashish07-cloud <172781723+ashish07-cloud@users.noreply.github.com>
2024-09-23 23:30:03 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,14 @@
+import user from './user.png'
+import logo from './logo.jpg'
+import search from './search.png'
+import map from './map.jpeg'
+
+
+
+
+export const assets ={
+    user,
+    logo,
+    search,
+    map,
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+
+
+const Searchbar = () => {
+  return (
+    <div className=" flex justify-center items-center">
+      <div className="w-full max-w-md mx-auto p-4">
+        <div className="relative">
+          <input
+            type="search"
+            placeholder="Search..."
+            className="w-full pl-10 pr-4 py-2 text-sm text-gray-700 border border-gray-300 rounded-md"
+          />
+          <img
+            src={assets.search}
+            alt="Search icon"
+            className="absolute top-1/2 right-3 transform -translate-y-1/2 w-6 h-6"
+          />
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Searchbar
